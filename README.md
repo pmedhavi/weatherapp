fa605b4b22f6030a21fc85d7daa5dc7056a7dab4
pmedhavi <medhhaviprakhar@gmail.com>
2023-07-30 20:46:55 +0530

    Sorting Visualizer

---
+++ README.md
@@ -0,0 +1,4 @@
+# weatherapp
+A simple Weather App that will fetch weather data of different cities from APIs using HTML5, CSS3 and JavaScript 
+
+HOISTED LINK :- https://merry-marzipan-168130.netlify.app/
---
+++ index.js
@@ -0,0 +1,54 @@
+const api = {
+    key: "b1bdec5c58a3e47f1c37bd3f1f493df0",
+    baseurl: "https://api.openweathermap.org/data/2.5/",
+};
+
+const searchBox = document.querySelector('.search-box');
+searchBox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if(evt.keyCode == 13) {
+        getResults(searchBox.value);
+    }
+}
+
+function getResults(query) {
+    fetch(
+        `${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`
+    ).then(
+        weather => {
+            return weather.json();
+        }
+    ).then(
+        displayResults
+    );
+}
+
+function displayResults(weather) {
+    // console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerHTML = `${weather.name}, ${weather.sys.country}`;
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerHTML = weather.weather[0].main;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerHTML = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;
+}
+
+function dateBuilder(d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
